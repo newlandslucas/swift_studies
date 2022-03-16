ed530e345f2a34bc6351c9f71b0095632500d817
Lucas Newlands <lucasnewlands@MacBook-Pro-de-Lucas.local>
2022-03-16 20:12:55 -0300

    feat: Tuplas

---
+++ Studies.playground/Pages/Tuplas.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+var coordenadas = (x: 1, y: 1)
+
+// Alterando o valor
+coordenadas = (x: 2, y: 2)
+coordenadas = (3, 3)
+
+//Acessando elementos pelo nome
+coordenadas.x
+coordenadas.y
+
+// Acessando elemento pelo índice
+coordenadas.0
+coordenadas.1
